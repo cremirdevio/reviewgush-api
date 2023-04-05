633868fdf0ca81ebc82e88545b86d537ac03b674
cremirdevio <josephajibodu@gmail.com>
2023-04-05 14:42:54 +0100

    chore: reviews table updated

--- database/migrations/2023_04_05_125536_create_reviews_table.php
+++ database/migrations/2023_04_05_125536_create_reviews_table.php
@@ -13,18 +13,37 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->text()->comment('Contain the text for the review');
-            $table->unsignedFloat('rating')->comment('Rating for the review');
+            $table->string('name')->comment('Name of the reviewer'); // required
+            $table->string('email')->comment('Email Address of the reviewer'); // required
+            $table->unsignedFloat('rating')->comment('Rating for the review'); // required
+            $table->text('message')->comment('Contain the text for the review'); // required
+
+            // Extras
+            $table->string('title_or_company')->comment('Company of the reviewer or Position');
             $table->foreignId('review_collection_id')
                 ->nullable()
                 ->comment('Foreign key to collections table')
                 ->constrained('review_collections')
                 ->cascadeOnUpdate()
                 ->nullOnDelete();
-            $table->string('platform')->comment('e.g Native(reviewgush), Google, Yelp, Facebook, etc');
-            $table->string('video')->comment('Video url for the review');
-            $table->string('image')->comment('Image url for the review');
+            $table->string('type')->comment('ReviewGush Inbox, External Integrations');
+            $table->boolean('reuse_consent')->default(true)->comment('Permission to use this testimonial across social channels and other marketing efforts');
+            $table->string('status')->comment('Active or Archived');
+
+            // Text Review
+            $table->string('avatar')->nullable()->comment('The reviewers avatar');
+            $table->string('image')->nullable()->comment('Image url for the review');
             $table->string('images')->nullable()->comment('List of other images for the review');
+
+            // Video Review
+            $table->string('video')->comment('Video url for the review');
+            $table->string('video_thumbnail')->nullable()->comment('Video thumbnail');
+            $table->text('excerpt')->comment('One sentence highlight to be displayed along with the video');
+
+            // Social Media Integrations
+            $table->string('integration_url')->nullable()->comment('Url of the post or comment from twitter, facebook, linkedin');
+            $table->string('integrations_type')->comment('e.g Twitter, Facebook, IG, Tiktok Google, Yelp, Facebook, etc');
+            $table->string('integrations_subtype')->nullable()->comment('Is it a comment, main post, twitter collection etc'); // not sure of this yet
             $table->timestamps();
         });
     }
