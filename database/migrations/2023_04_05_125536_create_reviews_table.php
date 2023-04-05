9f7b428297e7a68dd87316c2ff9cc2960e28d91c
cremirdevio <josephajibodu@gmail.com>
2023-04-05 14:03:25 +0100

    chore: reviews migrations table updated

--- database/migrations/2023_04_05_125536_create_reviews_table.php
+++ database/migrations/2023_04_05_125536_create_reviews_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->text()->comment('Contain the text for the review');
+            $table->unsignedFloat('rating')->comment('Rating for the review');
+            $table->foreignId('review_collection_id')
+                ->nullable()
+                ->comment('Foreign key to collections table')
+                ->constrained('review_collections')
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+            $table->string('platform')->comment('e.g Native(reviewgush), Google, Yelp, Facebook, etc');
+            $table->string('video')->comment('Video url for the review');
+            $table->string('image')->comment('Image url for the review');
+            $table->string('images')->nullable()->comment('List of other images for the review');
             $table->timestamps();
         });
     }
