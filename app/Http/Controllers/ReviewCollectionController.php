1333251306fbee5049cc6fbf95a02aabb3545c6a
cremirdevio <josephajibodu@gmail.com>
2023-04-05 13:54:54 +0100

    chore: collections domain created

---
+++ app/Http/Controllers/ReviewCollectionController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ReviewCollection;
+use Illuminate\Http\Request;
+
+class ReviewCollectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ReviewCollection $reviewCollection)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ReviewCollection $reviewCollection)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ReviewCollection $reviewCollection)
+    {
+        //
+    }
+}
